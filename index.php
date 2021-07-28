cd7ad0ae1831c414d9637f9c0768d8f90b2b48e5
Sampath Kumar C <sampathkumar.chinnaraju@photoninfotech.net>
2021-07-28 20:33:38 +0530

    function name changed

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title>Title</title>
 	<script>
 
-        function sendPersonalizedData() {
+        function sendCustomizedData() {
             var customData = '{ "type": "value", "value": {"url": "https:\/\/dsg-responsive-v2.mycustomizer.com","postMessage": {"designId": "1611087","imageURLs": ["https:\/\/dsg-responsive-v2.mycustomizer.com\/design\/1611087\/preview-200x200.png", "https:\/\/dsg-responsive-v2.mycustomizer.com\/design\/1611087\/front-400x400.png"],"data": {"Hand": "Left Hand","Shaft Material": "Steel ($125.00 ea.)",'+
             '"Set Make Up": "4-PW, SW","Shaft Manufacturer": "PING","Shaft": "1 STOCK AWT 2.0","Flex": "Regular","Length": "Standard","Lie": "Neutral (Black)","Custom Loft": "STANDARD","Grip Brand": "Golf Pride","Grip": "1 STOCK- Arccos Golf Pride Lite 360 Tour Velvet White","Grip Build": "Standard","Grip Installation": "Installed - Standard"},'+
             '"vendor": {"Hand": "Left Hand","Shaft Material": "Steel ($125.00 ea.)","Set Make Up": "4-PW, SW","Shaft Manufacturer": "PING",'+
@@ -29,7 +29,7 @@
 <div style="max-width:960px; margin: auto; text-align:center;">
 
 	<div style="margin-top: 50px;">
-		<button width="200px" height="70px" id="showToast" type="button" onclick="sendPersonalizedData()">Send Data</button>
+		<button width="200px" height="70px" id="showToast" type="button" onclick="sendCustomizedData()">Send Data</button>
 	</div>
 	<div id="editInformation"></div>
 </div>
