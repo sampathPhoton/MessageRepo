f8a2c2441a043be6ec83ff4cb95a9491700a6785
Sampath Kumar C <sampathkumar.chinnaraju@photoninfotech.net>
2021-07-28 18:14:27 +0530

    edit information updated

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<script>
 
         function sendPersonalizedData() {
-            var personalizedData = '{ "type": "value", "value": {"url": "https:\/\/dsg-responsive-v2.mycustomizer.com","postMessage": {"designId": "1611087","imageURLs": ["https:\/\/dsg-responsive-v2.mycustomizer.com\/design\/1611087\/preview-200x200.png", "https:\/\/dsg-responsive-v2.mycustomizer.com\/design\/1611087\/front-400x400.png"],"data": {"Hand": "Left Hand","Shaft Material": "Steel ($125.00 ea.)",'+
+            var customData = '{ "type": "value", "value": {"url": "https:\/\/dsg-responsive-v2.mycustomizer.com","postMessage": {"designId": "1611087","imageURLs": ["https:\/\/dsg-responsive-v2.mycustomizer.com\/design\/1611087\/preview-200x200.png", "https:\/\/dsg-responsive-v2.mycustomizer.com\/design\/1611087\/front-400x400.png"],"data": {"Hand": "Left Hand","Shaft Material": "Steel ($125.00 ea.)",'+
             '"Set Make Up": "4-PW, SW","Shaft Manufacturer": "PING","Shaft": "1 STOCK AWT 2.0","Flex": "Regular","Length": "Standard","Lie": "Neutral (Black)","Custom Loft": "STANDARD","Grip Brand": "Golf Pride","Grip": "1 STOCK- Arccos Golf Pride Lite 360 Tour Velvet White","Grip Build": "Standard","Grip Installation": "Installed - Standard"},'+
             '"vendor": {"Hand": "Left Hand","Shaft Material": "Steel ($125.00 ea.)","Set Make Up": "4-PW, SW","Shaft Manufacturer": "PING",'+
             '"Shaft Model": "1 STOCK AWT 2.0","Shaft Flex": "Regular","Custom Length": "Standard","Lie Angle": "Neutral (Black)","Custom Loft": "STANDARD","Grip Brand": "Golf Pride","Grip Model": "1 STOCK- Arccos Golf Pride Lite 360 Tour Velvet White","Grip Build": "Standard",'+
@@ -15,13 +15,13 @@
             var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
             if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-                window.webkit.messageHandlers.jsMessageHandler.postMessage(personalizedData);
+                window.webkit.messageHandlers.jsMessageHandler.postMessage(customData);
             } else {
-                PersonalizedDataInterface.sendPersonalizedData(personalizedData);
+                CustomizedDataInterface.sendCustomizedData(customData);
             }
         }
-        function editInformation(personalizedData) {
-            document.getElementById("editInformation").innerHTML = personalizedData
+        function editInformation(customData) {
+            document.getElementById("editInformation").innerHTML = customData
         }
     </script>
 </head>
